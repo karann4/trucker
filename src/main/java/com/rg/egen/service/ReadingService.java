1e66e5ff9ba586710bf21e1b05996e21015fead6
Karan <Karumbaiah4@gmail.com>
2020-05-09 18:38:55 -0400

    API module

---
+++ src/main/java/com/rg/egen/service/ReadingService.java
@@ -0,0 +1,15 @@
+package com.rg.egen.service;
+
+import com.rg.egen.entity.Reading;
+
+import java.util.List;
+
+public interface ReadingService {
+
+    List<Reading> findAll();
+
+    Reading findOne(String id);
+
+    Reading create(Reading reading);
+
+}
